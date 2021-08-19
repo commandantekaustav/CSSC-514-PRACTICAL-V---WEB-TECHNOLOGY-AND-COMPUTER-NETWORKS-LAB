bfbddc8e1914517783c870e8266d16b8bdbdef27
Inspirational_chaos <78654995+PreranaDebmallick@users.noreply.github.com>
2021-08-19 13:54:10 +0530

    Create Client.py
    eeee hihhihihi

---
+++ Computer-Networks/client/Client.py
@@ -0,0 +1,27 @@
+
+    
+import time, socket, sys
+ 
+#socket_server = socket.socket()
+host = socket.gethostname() 
+port = 8080 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
+
+name = input('Enter Friend\'s name: ')
+ 
+ 
+#client_socket.connect((server_host, sport))
+ 
+client_socket.send(name.encode())
+server_name = client_socket.recv(1024)
+server_name = server_name.decode()
+ 
+print(server_name,' has joined...')
+while True:
+    message = (client_socket.recv(1024)).decode()
+    print(server_name, ":", message)
+    message = input("Me : ")
+    client_socket.send(message.encode())
+    if(message=='bye'):
+        break
